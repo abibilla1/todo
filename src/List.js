3c24d5e7047db9c8edda6b2c942beac18d51ce1f
chatchat21 <66164387+chatchat21@users.noreply.github.com>
2020-09-08 20:59:00 +0600

    change

--- src/List.js
+++ src/List.js
@@ -4,20 +4,20 @@ import Item from './item';
 class List extends Component {
         state = {
             data: [
-                'Hit the gym',
-                'Pay bills',
-                'Meet George',
-                'Buy eggs',
-                'Read a boo',
-                'Organize office'
+                {id: 1,checked: false, label: 'Hit the gym'},
+                {id: 2,checked: true, label: 'Pay bills'},
+                {id: 3,checked: true, label: 'Meet George'},
+                {id: 4,checked: true, label: 'Buy eggs'},
+                {id: 5,checked: true, label: 'Read a book'},
+                {id: 6,checked: false, label: 'Organize office'}
             ]
         }
         render() {
             return (
                 <ul id='myUl'>
                     {this.state.data.map(
-                        (item) => {
-                            return <Item label={item}/>
+                        ({id,label,checked}) => {
+                            return <Item key={id} label={label} checked={checked}/>
                         }
                     )}
 
--- src/item.js
+++ src/item.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Item  = (props) => {
-    return <li>{props.label}</li>
+class Item extends Component {
+
+    // state = {
+    //     checked: true
+    // }
+    //
+    // onClick = (e) => {
+    //     this.setState((state) => {
+    //         return {checked: !state.checked}
+    //     })
+    // }
+
+    render() {
+        let className = '';
+        if (this.props.checked) {
+            className = 'checked';
+        }
+        return <li onClick={this.onClick} className={className}>{this.props.label}</li>
+    }
 }
 
 export default Item;
